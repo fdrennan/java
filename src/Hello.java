73998d834d1875de05dcd316d068cfd20db500e6
Freddy Drennan <drennanfreddy@gmail.com>
2018-03-07 14:24:02 -0700

    update

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        int myFirstNumber = 5;
+        String hello = "hello";
+        System.out.println(myFirstNumber);
+        System.out.println(hello);
+    }
+}
