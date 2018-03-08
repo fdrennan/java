604d8d60e7f6ba61082c79e48c17d12633aeec8e
Freddy Drennan <drennanfreddy@gmail.com>
2018-03-07 20:46:36 -0700

    update

---
+++ Chapter1/src/FirstMath.java
@@ -0,0 +1,9 @@
+public class FirstMath {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c;
+        c = Math.min(a, b);
+        System.out.println(c);
+    }
+}
